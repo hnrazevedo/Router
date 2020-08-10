ac134badbd6cdb978370b6c04ed858763319ede2
Henri Azevedo <hnrazevedo@gmail.com>
2020-08-10 13:16:34 -0300

    add order comment

--- examples/Routes/default.php
+++ examples/Routes/default.php
@@ -2,8 +2,22 @@
 
 use HnrAzevedo\Router\Router;
 
-
+/* Never access */
 Router::get('/my-account','User:my_account')->filter('User:user_in');
+
+
+Router::get('/{teste}',function(){
+    echo 'teste';
+});
+
+Router::get('/1',function(){
+    echo 1;
+});
+
+Router::get('/3',function(){
+    echo 3;
+});
+
 
 Router::get('/{parameter}/{otherparameter}', function($data){
     echo "Parameter 1:{$data['parameter']}, Parameter 2:{$data['otherparameter']}.";
